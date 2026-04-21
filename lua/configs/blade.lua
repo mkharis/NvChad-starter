0b72b21b6cb55d8219612579aea054ffa423dcd9
Mukhammad Kharis <081332210xxx@gmail.com>
2026-04-21 20:34:47 +0700

    fix blade for require nvim treesitter

--- lua/configs/blade.lua
+++ lua/configs/blade.lua
@@ -1,4 +1,4 @@
-local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+local parser_config = require("nvim-treesitter.parsers").get_parser_info()
 
 parser_config.blade = {
   install_info = {
